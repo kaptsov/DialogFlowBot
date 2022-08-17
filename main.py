03889915cf5962307d4e60e99589be7ab157f57c
alexanderkaptsov <kaptsovaa@gmail.com>
2022-08-17 21:56:58 +0400

    added Unknwn command handler

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
 import logging
+import os
+
 from environs import Env
-
-from telegram import __version__ as TG_VER
-
-try:
-    from telegram import __version_info__
-except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
-
-if __version_info__ < (20, 0, 0, "alpha", 1):
-    raise RuntimeError(
-        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
-        f"{TG_VER} version of this example, "
-        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
-    )
+from google.cloud import dialogflow
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
+        rf"Здравствуйте, {user.mention_html()}! Давайте пообщаемся.",
         reply_markup=ForceReply(selective=True),
     )
 
@@ -40,9 +25,47 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text("Help!")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+async def on_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    await update.message.reply_text(
+        detect_intent_texts(
+            project_id=os.getenv('DIALOG_FLOW_PROJECT_ID'),
+            session_id=update.effective_chat.id,
+            text=update.message.text,
+            language_code='ru-RU'
+            )
+        )
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Извините, команда не распознана.")
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("=" * 20)
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
 
 
 def main() -> None:
@@ -50,10 +73,16 @@ def main() -> None:
     env = Env()
     env.read_env()
 
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    on_message_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, on_message)
+
     application = Application.builder().token(env('TELEGRAM_TOKEN')).build()
+
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    application.add_handler(on_message_handler)
+    application.add_handler(unknown_handler)
     application.run_polling()
 
 
